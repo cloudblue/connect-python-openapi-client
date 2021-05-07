f5c41c34e46dc1cefcdf938e2cdbb9fad72b88f5
Francesco Faraone <francesco.faraone@ingrammicro.com>
2021-05-07 10:21:14 +0200

    fix invalid kwargs

--- tests/async_client/test_fluent.py
+++ tests/async_client/test_fluent.py
@@ -6,10 +6,7 @@ from connect.client import AsyncConnectClient
 @pytest.mark.asyncio
 async def test_async_get(async_mocker):
     url = 'https://localhost'
-    kwargs = {
-        'arg1': 'val1',
-    }
     mocked = async_mocker.patch.object(AsyncConnectClient, 'execute')
-    c = AsyncConnectClient('API_KEY', use_specs=False)
-    await c.get(url, **kwargs)
-    mocked.assert_awaited_once_with('get', url, **kwargs)
+    c = AsyncConnectClient('API_KEY', endpoint=url, use_specs=False)
+    await c.get(url)
+    mocked.assert_awaited_once_with('get', url)
