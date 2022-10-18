54a3aadcb7fa0a2685f8059b88d7b1cc99167ab1
Francesco Faraone <francesco.faraone@cloudblue.com>
2022-10-18 07:15:59 +0000

    LITE-21143 obfuscate api_key in cookies

--- connect/client/logger.py
+++ connect/client/logger.py
@@ -6,11 +6,18 @@ class RequestLogger:
     def __init__(self, file=sys.stdout):
         self._file = file
 
-    def obfuscate(self, value):
-        if value.startswith('ApiKey SU-'):
-            return value.split(':')[0] + '*' * 10
-        else:
-            return '*' * 20
+    def obfuscate(self, key, value):
+        if key in ('authorization', 'authentication'):
+            if value.startswith('ApiKey '):
+                return value.split(':')[0] + '*' * 10
+            else:
+                return '*' * 20
+        if key in ('cookie', 'set-cookie'):
+            if 'api_key="' in value:
+                start_idx = value.index('api_key="') + len('api_key="')
+                end_idx = value.index('"', start_idx)
+                return f'{value[0:start_idx + 2]}******{value[end_idx - 2:]}'
+        return value
 
     def log_request(self, method, url, kwargs):
         other_args = {k: v for k, v in kwargs.items() if k not in ('headers', 'json', 'params')}
@@ -26,8 +33,8 @@ class RequestLogger:
 
         if 'headers' in kwargs:
             for k, v in kwargs['headers'].items():
-                if k == 'Authorization':
-                    v = self.obfuscate(v)
+                if k.lower() in ('authorization', 'authentication', 'cookie'):
+                    v = self.obfuscate(k.lower(), v)
                 lines.append(f'{k}: {v}')
 
         if 'json' in kwargs:
@@ -45,6 +52,8 @@ class RequestLogger:
         ]
 
         for k, v in response.headers.items():
+            if k.lower() == 'set-cookie':
+                v = self.obfuscate(k.lower(), v)
             lines.append(f'{k}: {v}')
 
         if response.headers.get('Content-Type', None) == 'application/json':
